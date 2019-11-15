57094635a51f7ff9392862f67003c9030dbad9ae
zhouwl <zhouwl@missfress.cn>
2019-11-15 16:35:14 +0800

    代码格式化

--- lib/optl.lua
+++ lib/optl.lua
@@ -1,4 +1,3 @@
-
 -- 用于生成唯一随机字符串
 local random = require "resty-random"
 local stool = require "stool"
@@ -14,9 +13,11 @@ local config = cjson_safe.decode(config_dict:get("config")) or {}
 local config_version = 0
 
 --- 读取文件（全部读取/按行读取）
-local function readfile(_filepath,_ty)
-    local fd = io.open(_filepath,"r")
-    if fd == nil then return end
+local function readfile(_filepath, _ty)
+    local fd = io.open(_filepath, "r")
+    if fd == nil then
+        return
+    end
     if _ty == nil then
         local str = fd:read("*a") --- 全部内容读取
         fd:close()
@@ -34,13 +35,13 @@ end
 -- 默认写文件错误时，会将错误信息和_msg数据使用ngx.log写到错误日志中。
 -- ngx.log对写入的信息进行了大小控制，一些大数据情况理论上不用担心
 -- 自己调用时，_msg的内容大小需要自己进行控制
-local function writefile(_filepath,_msg,_ty)
+local function writefile(_filepath, _msg, _ty)
     _ty = _ty or "a+"
     -- w+ 覆盖 写文件方式默认是追加方式
     -- local fd = assert(io.open(_filepath,_ty),"writefile io.open error")
-    local fd,err = io.open(_filepath,_ty)
+    local fd, err = io.open(_filepath, _ty)
     if fd == nil then
-        ngx.log(ngx.ERR,"writefile msg : "..tostring(_msg),err)
+        ngx.log(ngx.ERR, "writefile msg : " .. tostring(_msg), err)
         return
     end -- 文件读取错误返回
     fd:write(tostring(_msg))
@@ -65,7 +66,7 @@ local function tableTostring(_obj)
             lua = lua .. "[" .. tableTostring(k) .. "]=" .. tableTostring(v) .. ",\n"
         end
         local metatable = getmetatable(_obj)
-            if metatable ~= nil and type(metatable.__index) == "table" then
+        if metatable ~= nil and type(metatable.__index) == "table" then
             for k, v in pairs(metatable.__index) do
                 lua = lua .. "[" .. tableTostring(k) .. "]=" .. tableTostring(v) .. ",\n"
             end
@@ -80,8 +81,10 @@ local function tableTostring(_obj)
 end
 
 local function stringTotable(_str)
-    if _str == nil then return end
-    local ret = loadstring("return ".._str)()
+    if _str == nil then
+        return
+    end
+    local ret = loadstring("return " .. _str)()
     return ret
 end
 
@@ -100,22 +103,22 @@ end
 local function guid(_num)
     _num = _num or 10
     return string.format('%s-%s',
-        random.token(_num),
-        random.token(_num)
+            random.token(_num),
+            random.token(_num)
     )
 end
 
 -- 设置token 并缓存2分钟
-local function set_token(_token,_t,_len)
+local function set_token(_token, _t, _len)
     _len = _len or 10
     local _lenNext = _len + 1
     _token = _token or guid(_len)
-    _t = _t or 2*60
-    local re = token_dict:add(_token,true,_t)  --- -- 缓存2分钟 非重复插入
+    _t = _t or 2 * 60
+    local re = token_dict:add(_token, true, _t)  --- -- 缓存2分钟 非重复插入
     if re then
         return _token
     else
-        return set_token(guid(_lenNext),_t,_lenNext)
+        return set_token(guid(_lenNext), _t, _lenNext)
     end
 end
 
@@ -127,57 +130,72 @@ end
 -- 说明：[_restr,_options]  _str 就是被匹配的内容
 -- eg "ip":["*",""]
 -- eg "hostname":[["www.abc.com","127.0.0.1"],"list"]
-local function remath(_str,_re_str,_options)
-    if _str == nil or _re_str == nil or _options == nil then return false end
+local function remath(_str, _re_str, _options)
+    if _str == nil or _re_str == nil or _options == nil then
+        return false
+    end
     if _options == "" then
         -- 纯字符串匹配 * 表示任意
         if _str == _re_str or _re_str == "*" then
             return true
         end
     elseif _options == "list" then
-        return stool.isInArrayTb(_str,_re_str)
+        return stool.isInArrayTb(_str, _re_str)
     elseif _options == "in" then
-        return stool.stringIn(_str,_re_str)
-    -- add new type
+        return stool.stringIn(_str, _re_str)
+        -- add new type
     elseif _options == "len" then
-        if type(_re_str) ~= "table" then return false end
+        if type(_re_str) ~= "table" then
+            return false
+        end
         local len_str = #_str
         if len_str >= _re_str[1] and len_str <= _re_str[2] then
             return true
         end
     elseif _options == "start_list" then
-        if type(_re_str) ~= "table" then return false end
-        for _,v in ipairs(_re_str) do
-            if stool.stringStarts(_str,v) then
+        if type(_re_str) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(_re_str) do
+            if stool.stringStarts(_str, v) then
                 return true
             end
         end
     elseif _options == "end_list" then
-        if type(_re_str) ~= "table" then return false end
-        for _,v in ipairs(_re_str) do
-            if stool.stringEnds(_str,v) then
+        if type(_re_str) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(_re_str) do
+            if stool.stringEnds(_str, v) then
                 return true
             end
         end
     elseif _options == "in_list" then
-        if type(_re_str) ~= "table" then return false end
-        for _,v in ipairs(_re_str) do
-            if stool.stringIn(_str,v) then
+        if type(_re_str) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(_re_str) do
+            if stool.stringIn(_str, v) then
                 return true
             end
         end
     elseif _options == "rein_list" then
-        if type(_re_str) ~= "table" then return false end
-        for _,v in ipairs(_re_str) do
-            if stool.stringIn(_str,string.upper(v)) then
+        if type(_re_str) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(_re_str) do
+            if stool.stringIn(_str, string.upper(v)) then
                 return true
             end
         end
     elseif _options == "dict" then
         --- 字典(dict) 匹配，o(1) 比序列(list)要好些， 字符串完全匹配
-        if type(_re_str) ~= "table" then return false end
+        if type(_re_str) ~= "table" then
+            return false
+        end
         local re = _re_str[_str]
-        if re == true then -- 需要判断一下 有可能是值类型的值
+        if re == true then
+            -- 需要判断一下 有可能是值类型的值
             return true
         end
     elseif _options == "@token@" then
@@ -189,8 +207,10 @@ local function remath(_str,_re_str,_options)
         end
     elseif _options == "cidr" then
         --- 基于cidr，用于匹配ip 是否在 ip段中
-        if type(_re_str) ~= "table" then return false end
-        for _,v in ipairs(_re_str) do
+        if type(_re_str) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(_re_str) do
 
             local cidr = require "cidr"
             local first_address, last_address = cidr.parse_cidr(v)
@@ -200,7 +220,7 @@ local function remath(_str,_re_str,_options)
             --// get the ip to decimal.
 
             if ip_num >= first_address and ip_num <= last_address then
-            --// judge if ip lies between the cidr.
+                --// judge if ip lies between the cidr.
                 return true
             end
         end
@@ -218,28 +238,28 @@ end
 -- 说明：[_restr,_options]  _str 就是被匹配的内容
 -- eg "ip":["*",""]
 -- eg "hostname":[["www.abc.com","127.0.0.1"],"table",false/true]
-local function remath_Invert(_str,_re_str,_options,_Invert)
+local function remath_Invert(_str, _re_str, _options, _Invert)
     if _Invert then
-        if not remath(_str,_re_str,_options) then
+        if not remath(_str, _re_str, _options) then
             return true
         end
     else
-        if remath(_str,_re_str,_options) then
+        if remath(_str, _re_str, _options) then
             return true
         end
     end
 end
 
 -- 传入 (remoteIp,ipfrom)
-local function loc_getRealIp(_remoteIp,_ipfrom)
+local function loc_getRealIp(_remoteIp, _ipfrom)
     local ipfrom = _ipfrom or {}
     if type(ipfrom.ips) ~= "table" then
         return _remoteIp
     end
-    if remath_Invert(_remoteIp,ipfrom.ips[1],ipfrom.ips[2],ipfrom.ips[3]) then
+    if remath_Invert(_remoteIp, ipfrom.ips[1], ipfrom.ips[2], ipfrom.ips[3]) then
         --- header 中key名称 - 需要转换成 _
         --local x = 'http_'..ngx_re_gsub(tostring(ipfrom.realipfrom),'-','_')
-        local x = 'http_'..ipfrom.realipfrom
+        local x = 'http_' .. ipfrom.realipfrom
         local ip = ngx_unescape_uri(ngx.var[x])
         if ip == "" then
             ip = _remoteIp
@@ -255,7 +275,7 @@ end
 -- _modrule = ["^[\\w]{6}$","jio",["cc",3],true]
 -- _modrule = ["true","@token@",["cctoken"],true]
 -- _modrule = ["^[\\w]{6}$","jio",["sign"],true]
-local function remath3(_tbMod,_modrule)
+local function remath3(_tbMod, _modrule)
     local _re_str = _modrule[1]
     local _options = _modrule[2]
     local _str = _tbMod[_modrule[3][1]]
@@ -267,28 +287,29 @@ local function remath3(_tbMod,_modrule)
     if type(_str) == "table" then
         if _ty == "end" then
             _ty = #_str
-            if remath_Invert(_str[_ty],_re_str,_options,_Invert) then
+            if remath_Invert(_str[_ty], _re_str, _options, _Invert) then
                 return true
             end
         elseif _ty == "all" then
-            for _,v in ipairs(_str) do
-                if remath_Invert(v,_re_str,_options,_Invert) then
+            for _, v in ipairs(_str) do
+                if remath_Invert(v, _re_str, _options, _Invert) then
                     return true
                 end
             end
-        else -- table 中的某一个
+        else
+            -- table 中的某一个
             -- 超出范围判断
             if _ty > #_str then
                 _ty = 1
             else
                 _ty = #_str
             end
-            if remath_Invert(_str[_ty],_re_str,_options,_Invert) then
+            if remath_Invert(_str[_ty], _re_str, _options, _Invert) then
                 return true
             end
         end
     else
-        if remath_Invert(_str,_re_str,_options,_Invert) then
+        if remath_Invert(_str, _re_str, _options, _Invert) then
             return true
         end
     end
@@ -297,7 +318,7 @@ end
 -- 增加 post_form的规则匹配
 -- _tbMod : [["form name","file name","file type","file msg"],...]
 -- _modrule : ["\\.(jpg|jpeg|png|webp|gif)$","jio",["image0",2],true/false/flase]
-local function remath_form(_tbMod,_modrule)
+local function remath_form(_tbMod, _modrule)
     if type(_tbMod) ~= "table" or type(_modrule) ~= "table" then
         return false
     end
@@ -309,10 +330,10 @@ local function remath_form(_tbMod,_modrule)
     if type(_form_n) ~= "number" or _form_n < 1 or _form_n > 4 then
         return false
     end
-    for _,v in ipairs(_tbMod) do
+    for _, v in ipairs(_tbMod) do
         if v[1] == _form_name then
             local _str = v[_form_n]
-            if remath_Invert(_str,_re_str,_options,_Invert) then
+            if remath_Invert(_str, _re_str, _options, _Invert) then
                 return true
             end
         end
@@ -327,23 +348,23 @@ end
 -- _modRule = ["^[\\w]{6}$","jio",["cc",3],true]
 -- _modRule = ["true","@token@",["cctoken"],true]
 -- _modRule = ["^[\\w]{6}$","jio",["sign"]]
-local function action_remath(_modName,_modRule,_base_Msg)
+local function action_remath(_modName, _modRule, _base_Msg)
 
     if _modName == nil or type(_base_Msg) ~= "table" or type(_modRule) ~= "table" then
         return false
     end
     if type(_base_Msg[_modName]) == "table" then
         if _modName == "post_form" then
-            if remath_form(_base_Msg[_modName],_modRule) then
+            if remath_form(_base_Msg[_modName], _modRule) then
                 return true
             end
         else
-            if remath3(_base_Msg[_modName],_modRule) then
+            if remath3(_base_Msg[_modName], _modRule) then
                 return true
             end
         end
     else
-        if remath_Invert(_base_Msg[_modName],_modRule[1],_modRule[2],_modRule[3]) then
+        if remath_Invert(_base_Msg[_modName], _modRule[1], _modRule[2], _modRule[3]) then
             return true
         end
     end
@@ -352,10 +373,11 @@ end
 -- 对 or 规则list 进行判断
 -- _or_list = ["referer",["baidu","in",true]]
 -- _or_list = ["args",["^[\\w]{6}$","jio",["cc",3],true],"and"]
-local function or_remath(_or_list,_basemsg)
+local function or_remath(_or_list, _basemsg)
     -- or 匹配 任意一个为真 则为真
-    for _,v in ipairs(_or_list) do
-        if action_remath(v[1],v[2],_basemsg) then -- 真
+    for _, v in ipairs(_or_list) do
+        if action_remath(v[1], v[2], _basemsg) then
+            -- 真
             return true
         end
     end
@@ -372,26 +394,33 @@ end
 -- _app_list = ["args",["true","@token@",["cctoken"],true]]
 -- _app_list = ["headers",["^[\\w]{6}$","jio",["sign"],true]]
 --  post_form 表单
-local function re_app_ext(_app_list,_basemsg)
-    if type(_app_list) ~= "table" then return false end
+local function re_app_ext(_app_list, _basemsg)
+    if type(_app_list) ~= "table" then
+        return false
+    end
     local list_cnt = #_app_list
     local tmp_or = {}
-    for i,v in ipairs(_app_list) do
+    for i, v in ipairs(_app_list) do
         if v[3] == "or" then
-            table.insert(tmp_or,v)
+            table.insert(tmp_or, v)
             if i == list_cnt then
-                return or_remath(tmp_or,_basemsg)
+                return or_remath(tmp_or, _basemsg)
             end
         else
-            if #tmp_or == 0 then -- 前面没 or
-                if action_remath(v[1],v[2],_basemsg) then -- 真
+            if #tmp_or == 0 then
+                -- 前面没 or
+                if action_remath(v[1], v[2], _basemsg) then
+                    -- 真
                     -- continue
-                else -- 假 跳出
+                else
+                    -- 假 跳出
                     return false
                 end
-            else -- 一组 or 计算
+            else
+                -- 一组 or 计算
                 table.insert(tmp_or, v)
-                if or_remath(tmp_or,_basemsg) then -- 真
+                if or_remath(tmp_or, _basemsg) then
+                    -- 真
                     -- continue
                 else
                     return false
@@ -406,10 +435,12 @@ end
 --- 拦截计数
 -- set失败未处理
 local function set_count_dict(_key)
-    if _key == nil then return end
-    local re, err = count_dict:incr(_key,1)
+    if _key == nil then
+        return
+    end
+    local re, err = count_dict:incr(_key, 1)
     if re == nil then
-       count_dict:set(_key,1)
+        count_dict:set(_key, 1)
     end
 end
 
@@ -418,18 +449,18 @@ local function ngx_find(_str)
     -- str = string.gsub(str,"@ngx_time@",ngx.time())
     -- ngx.re.gsub 效率要比string.gsub要好一点，参考openresty最佳实践
     _str = tostring(_str)
-    _str = ngx_re_gsub(_str,"@ngx_localtime@",tostring(ngx.localtime()))
+    _str = ngx_re_gsub(_str, "@ngx_localtime@", tostring(ngx.localtime()))
 
     -- string.find 字符串 会走jit,所以就没有用ngx模块
     -- 当前情况下，对token仅是全局替换一次，请注意
-    if string.find(_str,"@token@") ~= nil then
-        _str = ngx_re_gsub(_str,"@token@",tostring(set_token()))
+    if string.find(_str, "@token@") ~= nil then
+        _str = ngx_re_gsub(_str, "@token@", tostring(set_token()))
     end
     return _str
 end
 
 --- 对not table 类型的数据 进行 ngx_find
-local function sayHtml_ext(_html,_find_type,_content_type)
+local function sayHtml_ext(_html, _find_type, _content_type)
     --ngx.header.content_type = "text/html"
     if _html == nil then
         _html = "_html is nil"
@@ -450,7 +481,7 @@ local function sayHtml_ext(_html,_find_type,_content_type)
 end
 
 --- ngx_find 无条件使用
-local function sayFile(_filename,_header)
+local function sayFile(_filename, _header)
     --ngx.header.content_type = "text/html"
     --local str = readfile(Config.base.htmlPath..filename)
     local str = readfile(_filename) or "filename error"
@@ -470,68 +501,74 @@ end
 
 --- 请求相关 正常使用阶段在access/rewrite set没测试过
 
-    --- 获取单个args值
-    local function get_argsByName(_name)
-        if _name == nil then return "" end
-        local x = 'arg_'.._name
-        local _name = ngx_unescape_uri(ngx.var[x])
-        return _name
-        -- local args_name = ngx.req.get_uri_args()[_name]
-        -- if type(args_name) == "table" then args_name = args_name[1] end
-        -- return ngx_unescape_uri(args_name)
+--- 获取单个args值
+local function get_argsByName(_name)
+    if _name == nil then
+        return ""
     end
+    local x = 'arg_' .. _name
+    local _name = ngx_unescape_uri(ngx.var[x])
+    return _name
+    -- local args_name = ngx.req.get_uri_args()[_name]
+    -- if type(args_name) == "table" then args_name = args_name[1] end
+    -- return ngx_unescape_uri(args_name)
+end
 
-    --- 获取单个post值 非POST方法使用会异常
-    local function get_postByName(_name)
-        if _name == nil then return "" end
-        --ngx.req.read_body()
-        local posts_name = ngx.req.get_post_args()[_name]
-        if type(posts_name) == "table" then posts_name = posts_name[1] end
-        return ngx_unescape_uri(posts_name)
+--- 获取单个post值 非POST方法使用会异常
+local function get_postByName(_name)
+    if _name == nil then
+        return ""
     end
+    --ngx.req.read_body()
+    local posts_name = ngx.req.get_post_args()[_name]
+    if type(posts_name) == "table" then
+        posts_name = posts_name[1]
+    end
+    return ngx_unescape_uri(posts_name)
+end
 
-    --- 获取所有POST参数（包含表单）
-    local function get_post_all()
-        --ngx.req.read_body()
-        local data = ngx.req.get_body_data() -- ngx.req.get_post_args()
-        if not data then
-            local datafile = ngx.req.get_body_file()
-            if datafile then
-                local fh, err = io.open(datafile, "r")
-                if fh then
-                    fh:seek("set")
-                    data = fh:read("*a")
-                    fh:close()
-                end
+--- 获取所有POST参数（包含表单）
+local function get_post_all()
+    --ngx.req.read_body()
+    local data = ngx.req.get_body_data() -- ngx.req.get_post_args()
+    if not data then
+        local datafile = ngx.req.get_body_file()
+        if datafile then
+            local fh, err = io.open(datafile, "r")
+            if fh then
+                fh:seek("set")
+                data = fh:read("*a")
+                fh:close()
             end
         end
-        return ngx_unescape_uri(data)
     end
+    return ngx_unescape_uri(data)
+end
 
-    local function get_table(_tb)
-        if _tb == nil then
-            return ""
-        end
-        local tb_args = {}
-        for k,v in pairs(_tb) do
-            if type(v) == "table" then
-                local tmp_v = {}
-                for i,vv in ipairs(v) do
-                    if vv == true then
-                        vv=""
-                    end
-                    table.insert(tmp_v,vv)
+local function get_table(_tb)
+    if _tb == nil then
+        return ""
+    end
+    local tb_args = {}
+    for k, v in pairs(_tb) do
+        if type(v) == "table" then
+            local tmp_v = {}
+            for i, vv in ipairs(v) do
+                if vv == true then
+                    vv = ""
                 end
-                v = table.concat(tmp_v,",")
-            elseif v == true then
-                v= ""
+                table.insert(tmp_v, vv)
             end
-            table.insert(tb_args,v)
+            v = table.concat(tmp_v, ",")
+        elseif v == true then
+            v = ""
         end
-        return table.concat(tb_args,",")
+        table.insert(tb_args, v)
     end
+    return table.concat(tb_args, ",")
+end
 
-local optl={}
+local optl = {}
 
 -- 配置json
 optl.config = config
